dcef2c12b36e89496ac298f697376e979fbe74e0
Luo Sheng <luosheng1986@gmail.com>
2014-08-04 13:31:53 +0800

    Initial Commit

---
+++ Netease Music Wrapper/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Netease Music Wrapper
+//
+//  Created by Luo Sheng on 14-8-4.
+//  Copyright (c) 2014年 Pop Tap. All rights reserved.
+//
+
+import Cocoa
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+                            
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification?) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification?) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Netease Music Wrapper/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  Netease Music Wrapper
+//
+//  Created by Luo Sheng on 14-8-4.
+//  Copyright (c) 2014年 Pop Tap. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
